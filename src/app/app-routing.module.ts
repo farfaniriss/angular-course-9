7bea277f3d6545b9ac03f82c47e54dd639aef778
farfaniriss <iris.farfan.c@gmail.com>
2018-11-21 15:49:43 -0500

    auth0 authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./components/home/home.component";
+import { PricesComponent } from "./components/prices/prices.component";
+import { ProtectedComponent } from "./components/protected/protected.component";
+
+import { AuthGuardService } from "./services/auth-guard.service";
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'prices', component: PricesComponent },
+  { 
+    path: 'protected', 
+    component: ProtectedComponent,
+    canActivate: [AuthGuardService]
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from "@angular/router";
+
+import { AuthService } from "./auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(private auth: AuthService) { }
+
+  canActivate(next:ActivatedRouteSnapshot, state:RouterStateSnapshot){
+    
+    console.log(next);
+
+    if (this.auth.isAuthenticated){
+      console.log('You are authenticated')
+      return true;
+    }
+    else {
+      console.error('blocked by guard')
+      return false;
+    }
+  }
+}
